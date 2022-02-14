46d0d64c6dbf5dcdb0e8126c60bd3cfe870d0d83
nukesaku <nextniconico@gmail.com>
2022-02-14 23:55:35 +0900

    [Chapter5]:+1:layoutに共有ページを追加

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,10 @@
 module ApplicationHelper
+  SITE_NAME = 'Baukis2'.freeze
+  def document_title
+    if @title.present?
+      "#{@title} - #{SITE_NAME}"
+    else
+      SITE_NAME
+    end
+  end
 end
