e121c74113d98389a6f1e2b4e08ba4696c0cb05c
nukesaku <nextniconico@gmail.com>
2022-04-30 16:31:37 +0900

    [9.2]リソースベースのルーティング

--- app/controllers/admin/base.rb
+++ app/controllers/admin/base.rb
@@ -1,7 +1,7 @@
 class Admin::Base < ApplicationController
   private def current_administrator
     if session[:administrator_id]
-      @current_staff_member ||= StaffMember.find_by(id: session[:staff_member_id])
+      @current_staff_member ||= Administrator.find_by(id: session[:administrator_id])
     end
   end
   
