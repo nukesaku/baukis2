bb20275728d8fbe979b81a753e64c9d76c8abb04
nukesaku <nextniconico@gmail.com>
2022-08-15 19:42:14 +0900

    [ex][5.2]許可IPアドレスの管理

--- app/models/allowed_source.rb
+++ app/models/allowed_source.rb
@@ -1,4 +1,18 @@
 class AllowedSource < ApplicationRecord
+  attr_accessor :last_octet, :_destroy
+
+  before_validation do
+    if last_octet
+      self.last_octet.strip!
+      if last_octet == "*"
+        self.octet4 = 0
+        self.wildcard = true
+      else
+        self.octet4 = last_octet
+      end
+    end
+  end
+
   validates :octet1, :octet2, :octet3, :octet4, presence: true,
             numericality: { only_integer: true, allow_blank: true }, # numericalityタイプのバリデーションは、変換前の値に対して行われるので、正しくエラー判定される。
             inclusion: { in: 0..255, allow_blank: true } # inclusionタイプのバリデーションは、"XYZ"のような文字列がこれらの属性に代入されると、整数0に変換されてしまう。
@@ -6,6 +20,16 @@ class AllowedSource < ApplicationRecord
             uniqueness: {
               scope: [ :namespace, :octet1, :octet2, :octet3 ], allow_blank: true
             } # 5つの属性の組み合わせに関してuniquenessタイプのバリデーションが実施される。
+  validates :last_octet,
+            inclusion: { in: (0..255).to_a.map(&:to_s) + [ "*" ], allow_blank: true }
+
+  after_validation do
+    if last_octet
+      errors[:octet4].each do |message|
+        errors.add(:last_octet, message)
+      end
+    end
+  end
 
   def ip_address=(ip_address)
     octets = ip_address.split(".")
